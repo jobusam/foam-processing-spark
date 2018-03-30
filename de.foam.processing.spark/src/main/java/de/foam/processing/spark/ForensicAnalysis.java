e816899ab65d1d1649ac83c9a8631edd90993ee3
jobusam <jbusam.development@gmail.com>
2018-03-30 13:26:21 +0200

    Save analysis results to one file and update scripts to delete output directory before execution

--- de.foam.processing.spark/src/main/java/de/foam/processing/spark/ForensicAnalysis.java
+++ de.foam.processing.spark/src/main/java/de/foam/processing/spark/ForensicAnalysis.java
@@ -6,7 +6,10 @@ import java.util.List;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.input.PortableDataStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.foam.processing.spark.common.FindDuplicates;
 import de.foam.processing.spark.hashing.Hashing;
@@ -23,15 +26,16 @@ import scala.Tuple2;
  */
 public class ForensicAnalysis {
 
-	// private static String dataDir = "/mnt/ssd/apps/data";
-	private static String dataDir = "/data";
+	private static String dataDir;
+	private static String outputDir;
+	private static final Logger LOGGER = LoggerFactory.getLogger(ForensicAnalysis.class);
 
 	public static void main(String[] args) {
 
-		if (args.length == 1 && args[0] != null && Paths.get(args[0]).toFile().isDirectory()) {
-			dataDir = args[0];
+		boolean validParams = validateInputParams(args);
+		if (!validParams) {
+			System.exit(1);
 		}
-		System.out.println(String.format("Data directory for analysis is %s", dataDir));
 
 		JavaSparkContext executionContext = createExecutionContext();
 
@@ -42,7 +46,8 @@ public class ForensicAnalysis {
 		try {
 			JavaPairRDD<String, String> hashesWithFileNames = Hashing.hashFiles(filesWithBinContent);
 			JavaPairRDD<String, String> results = FindDuplicates.filterForDuplicates(hashesWithFileNames);
-			printResults(results);
+			// printResults(results);
+			saveResults(results);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -50,6 +55,39 @@ public class ForensicAnalysis {
 
 		executionContext.stop();
 
+	}
+
+	/**
+	 * Check and set input parameters. For every execution an Input and Output
+	 * directory must be given!
+	 * 
+	 * @param args
+	 * @return true if the parameters are valid
+	 */
+	private static boolean validateInputParams(String[] args) {
+		if (args.length == 2) {
+			// Normally for input directory it's possible to check if path is a directory.
+			// But this doesn't work in case the directory is located in HDFS
+			if (args[0] != null && Paths.get(args[0]) != null) {
+				if (args[1] != null && Paths.get(args[1]) != null) {
+					dataDir = args[0];
+					outputDir = args[1];
+					LOGGER.info("InputDirectory is {}", dataDir);
+					LOGGER.info("OuputDirectory for Results is {}", outputDir);
+					return true;
+				} else {
+					LOGGER.error(
+							"<OutputDirectory> is invalid. Stop execution. Usage example: ForensicAnalysis <InputDirectory> <OuputDirectory>");
+				}
+			} else {
+				LOGGER.error(
+						"<InputDirectory> is invalid. Stop execution. Usage example: ForensicAnalysis <InputDirectory> <OuputDirectory>");
+			}
+		} else {
+			LOGGER.error(
+					"InputDirectory and OutputDirectory are missing!!!! Usage example: ForensicAnalysis <InputDirectory> <OuputDirectory>");
+		}
+		return false;
 	}
 
 	public static JavaSparkContext createExecutionContext() {
@@ -60,6 +98,25 @@ public class ForensicAnalysis {
 		// JavaSparkContext#wholeTextFiles)
 		jsc.hadoopConfiguration().set("mapreduce.input.fileinputformat.input.dir.recursive", "true");
 		return jsc;
+	}
+
+	/**
+	 * Save the resulting RDD into one single Text File!
+	 * 
+	 * @param results
+	 */
+	public static void saveResults(JavaPairRDD<String, String> results) {
+
+		Function<Tuple2<String, String>, String> textFormatter = (Tuple2<String, String> input) -> {
+			String head = String.format("SHA512-Hash: %s \n", input._1());
+			String body = input._2().replace(";", "\n");
+			return head + body + "\n\n";
+		};
+
+		results.map(textFormatter) // -
+				// for an small data set coalesce is ok, but review for large data!!!!
+				.coalesce(1)// -
+				.saveAsTextFile(outputDir);
 	}
 
 	public static void printResults(JavaPairRDD<String, String> results) {
