0c7a45492728d4d25cb94d868e6935f1155f0d64
jbusam <jbusam.development@gmail.com>
2018-10-18 22:58:26 +0200

    Remove unused code

--- spark.app.execution/remote/startApp.sh
+++ spark.app.execution/remote/startApp.sh
@@ -8,7 +8,7 @@ SPARK_APP_THIRDPARTY_LOCATION="processing/processing.spark-thirdparty-0.0.2-SNAP
 SPARK_APP_MAIN_CLASS="de.foam.processing.spark.ForensicAnalysis"
 
 HBASE_SITE_XML_FILE="/etc/hbase/conf/hbase-site.xml"
-HDFS_LARGE_FILE_DIR="/user/hdtest/ubuntuImageV.1.0/"
+HDFS_LARGE_FILE_DIR="/user/hdtest/forensicData/"
 
 #execute application via spark-submit
 spark-submit --master yarn --deploy-mode client --class $SPARK_APP_MAIN_CLASS --jars $SPARK_APP_THIRDPARTY_LOCATION $SPARK_APP_LOCATION $HBASE_SITE_XML_FILE $HDFS_LARGE_FILE_DIR
